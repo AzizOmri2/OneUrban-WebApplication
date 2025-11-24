80e3838027b260248e353fb03327b3aa43ce8350
Med Aziz Omri <95851261+AzizOmri2@users.noreply.github.com>
2025-11-24 18:23:59 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,14 +2,8 @@
 
 use App\Kernel;
 
-require dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-// Use environment variables directly
 return function (array $context) {
-    $env = $_SERVER['APP_ENV'] ?? 'prod';
-    $debug = (bool) ($_SERVER['APP_DEBUG'] ?? ($env !== 'prod'));
-
-    return new Kernel($env, $debug);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-?>
